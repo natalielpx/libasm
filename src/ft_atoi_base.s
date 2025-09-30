1e1eca49e47cad027196d380263b3e5eb4cfe4b1
Natalie LAM <nlam@f1br7s14.clusters.42paris.fr>
2025-09-30 21:55:33 +0200

    FIX(atoi_base) Fix negation bug
    Change storage of negative sign from rdx to rbx
    because mul clobbers both rax, rdx
    thus clobbering the negative sign in rdx

--- src/ft_atoi_base.s
+++ src/ft_atoi_base.s
@@ -4,7 +4,7 @@
 ;  Purpose   : converts string argument to base 10 integer and returns it
 ;  Args      : rdi - string, rsi - base
 ;  Returns   : base 10 integer
-;  Clobbers  : rax, rcx, rdx, rdi ,rsi, r8, r9, r10
+;  Clobbers  : rax, rcx, rbx, rdi ,rsi, r8, r9, r10
 ;  Arch      : x86-64 Linux (System V ABI)
 ; ============================================================================================ ;
 
@@ -14,7 +14,8 @@ default rel
 extern  __errno_location
 
 segment .rodata
-    digits db '0123456789abcdef', 0
+    digits_a db '0123456789abcdef', 0
+    digits_A db '0123456789ABCDEF', 0
 
 segment .text
     global  ft_atoi_base
@@ -23,6 +24,7 @@ ft_atoi_base:
 
 ; ===== INITIALISATION =====
     xor rax, rax
+    push rbx
 
 ; ===== PARSING =====
 
@@ -34,7 +36,7 @@ ft_atoi_base:
 
 .skip_white:
     movzx r8, byte [rdi]
-    cmp r8, 0x00
+    cmp r8, 0
     je .return
     cmp r8, ' '
     je .repeat_skip_white
@@ -47,25 +49,29 @@ ft_atoi_base:
     jmp .skip_white
 
 .check_neg:
-    xor rdx, rdx
+    xor rbx, rbx
     cmp r8, '-'
     jne .valid_digit
-    mov rdx, -1
+    mov rbx, -1
     inc rdi
 
 .valid_digit:
-    lea r9, [digits]
+    lea r9, [digits_a]
+    lea r10, [digits_A]
     xor rcx, rcx
 .repeat_valid_digit:
     movzx r8, byte [rdi]
     cmp r8, 0x00
-    je .return
-    movzx r10, byte [r9 + rcx]
-    cmp r8, r10
+    je .negate
+    movzx r11, byte [r9 + rcx]
+    cmp r8, r11
+    je .obtain_value
+    movzx r11, byte [r10 + rcx]
+    cmp r8, r11
     je .obtain_value
     inc rcx
     cmp rcx, rsi
-    jge .return
+    jge .negate
     jmp .repeat_valid_digit
 
 ; ===== OPERATIONS =====
@@ -73,73 +79,32 @@ ft_atoi_base:
 .obtain_value:
     cmp r8, 'a'
     jge .minus_a
+    cmp r8, 'A'
+    jge .minus_A
     sub r8, '0'
     jmp .calculate
 .minus_a:
     sub r8, 'a'
     add r8, 10
     jmp .calculate
+.minus_A:
+    sub r8, 'A'
+    add r8, 10
+    jmp .calculate
 
 .calculate:
-    mul rsi
+    mul rsi             ; clobbers rdx !!!
     add rax, r8
     inc rdi
     jmp .valid_digit
 
 .negate:
-    test rdx, rdx
+    test rbx, rbx
     jz .return
     neg rax
 
 ; ===== RETURN =====
 
 .return:
+    pop rbx
     ret
-
-; ; ===== FUNCTION(S) =====
-
-; convert:
-
-; ; ===== INITIALISATION =====
-;     movzx r8, byte [rdi]
-;     cmp r8, 0x00
-;     je .return
-
-; ; ===== PARSING =====
-
-; .valid_digit:
-;     lea r9, [digits]
-;     xor rcx, rcx
-; .repeat_valid_digit:
-;     movzx r10, byte [r9 + rcx]
-;     cmp r8, 0x00
-;     je .return
-;     cmp r8, r10
-;     je .obtain_value
-;     inc rcx
-;     cmp rcx, rsi
-;     jge .return
-;     jmp .repeat_valid_digit
-
-; ; ===== OPERATIONS =====
-
-; .obtain_value:
-;     cmp r8, 'a'
-;     jge .minus_a
-;     sub r8, '0'
-;     jmp .calculate
-; .minus_a:
-;     sub r8, 'a'
-;     add r8, 10
-;     jmp .calculate
-
-; .calculate:
-;     mul rsi
-;     add rax, r8
-;     inc rdi
-;     call convert
-
-; ; ===== RETURN =====
-
-; .return:
-;     ret
