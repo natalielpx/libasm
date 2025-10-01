93e51462be64891748142b0c46576cb94ef0f86b
Natalie LAM <nlam@f1br7s16.clusters.42paris.fr>
2025-10-01 12:59:32 +0200

    TEST(bonus) Add null arguments for list functions

--- test/tester_bonus.c
+++ test/tester_bonus.c
@@ -28,22 +28,29 @@ int main(void) {
 
 
 	printf("\n===== FT_LIST_PUSH_FRONT =====\n");
+	ft_list_push_front(NULL, "0");
+
 	t_list ** lst = malloc(sizeof(*lst));
 	for (int i = 0; i < N; ++i) {
 		char * data = malloc(sizeof(*data));
 		*data = i + 1 + '0';
 		ft_list_push_front(lst, data);
-		printf("pushed: %c\n", *data);
+		printf("pushed data: %c\n", *data);
 	}
     printf("==============================\n");
 
 
     printf("\n===== FT_LIST_SIZE =====\n");
+	ft_list_size(NULL);
+
 	printf("list size: %d\n", ft_list_size(*lst));
     printf("========================\n");
 
 
 	printf("\n===== FT_LIST_SORT =====\n");
+	ft_list_sort(lst, NULL);
+	ft_list_sort(NULL, ft_strcmp);
+
 	ft_list_sort(lst, ft_strcmp);
 	for (t_list * tmp = *lst; tmp; tmp = tmp->next)
 		printf("%c\n", (*(char *)tmp->data));
@@ -51,7 +58,11 @@ int main(void) {
 
 
 	printf("\n===== FT_LIST_REMOVE_IF =====\n");
-	ft_list_remove_if(lst, ";", ft_strcmp, free);
+	ft_list_remove_if(NULL, "4", ft_strcmp, free);
+	ft_list_remove_if(lst, "4", NULL, free);
+	ft_list_remove_if(lst, "4", ft_strcmp, NULL);
+
+	ft_list_remove_if(lst, "4", ft_strcmp, free);
 	for (t_list * tmp = *lst; tmp; tmp = tmp->next)
 		printf("%c\n", (*(char *)tmp->data));
     printf("=============================\n");
