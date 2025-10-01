444c3837a024fd40355fed3a7eb11c3ef924e944
natalie <natalielpx@gmail.com>
2025-10-01 20:19:23 +0200

    REF(push_front) Avoid rearranging registers

--- src/ft_list_push_front.s
+++ src/ft_list_push_front.s
@@ -4,12 +4,16 @@
 ;  Purpose   : returns number of elements in linked list passed to it
 ;  Args      : rdi - address of address of beginning of list, rsi - address of data
 ;  Returns   : void
-;  Clobbers  : rax
 ;  Arch      : x86-64 Linux (System V ABI)
 ; ============================================================================================ ;
 
+%define	INT	dword
+
+ENOMEM	equ 12
+
 default	rel
 extern	malloc
+extern	__errno_location
 
 ; ===== Structure of expected address of list passed =====
 ; typedef struct s_list {
@@ -17,7 +21,7 @@ extern	malloc
 ;     struct s_list *next;
 ; } t_list;
 
-segment .text
+section .text
 global	ft_list_push_front
 
 ft_list_push_front:
@@ -25,38 +29,47 @@ ft_list_push_front:
 ; ===== CHECK =====
 
 	; --- check if null ---
-	test rdi, rdi
-	jz .return
+	; rdi = begin_list
+	test rdi, rdi	; if !begin_list
+	jz .return		; return
 
 ; ===== MEMORY ALLOCATION =====
 
 	; --- allocate memory for new element ---
-	push qword rdi
-	push qword rsi
-	mov rdi, 16
+	push qword rdi	; save begin_list to stack
+	push qword rsi	; save data to stack
+	mov rdi, 16		; sizeof(t_list) = 16
+	; void *malloc(size_t size);
+	; rdi = sizeof(t_list) = 16
 	call malloc wrt ..plt
-	pop rsi
-	pop rdi
+	; rax = new (pointer to allocated memory)
+	pop rsi			; rsi = data
+	pop rdi			; rdi = begin_list
 
 	; --- check if malloc failed ---
 	test rax, rax	; if malloc returned null
-	jz .return		; return
+	jz .error		; return
 
 ; ===== PUSH TO FRONT =====
 
-	; --- rearrange registers ---
-	mov rdx, rax
-	mov rax, rdi
-	mov rcx, [rdi]
-
-	; --- fill new element ---
-	mov [rdx], rsi
-	mov [rdx + 8], rcx
+	; --- save 1st element ---
+	mov rdx, [rdi]	; rdx = 1st element (*begin_list)
 
 	; --- set new element as begin ---
-	mov [rax], rdx
+	mov [rdi], rax	; *begin_list = new
+	
+	; --- fill new element ---
+	mov [rax], rsi		; new->data = data
+	mov [rax + 8], rdx	; new->next = 1st element
 
 ; ===== RETURN =====
 
 .return:
+	; returns void
 	ret
+
+.error:
+	; int * __errno_location(void)
+    call __errno_location wrt ..plt
+    mov [rax], INT ENOMEM	; *errno (4 bytes) = error number
+	jmp .return
