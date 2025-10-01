c2c48abbbd17a56cb3b18a7298b9cb6e0ea2d702
natalie <natalielpx@gmail.com>
2025-10-01 18:20:29 +0200

    FIX(ft_read/ft_write) Push errno value to stack
    Storing errno value in a register does not guarantee its value
    Calling __errno_location might clobber its value
    Pushing ensures that the correct errno value is set

--- src/ft_read.s
+++ src/ft_read.s
@@ -4,7 +4,6 @@
 ;  Purpose   : Immitates (man 2 read)
 ;  Args      : rdi - input fd, rsi - ptr to buf, rdx - number of chars to read
 ;  Returns   : rax = number of chars written
-;  Clobbers  : rax
 ;  Arch      : x86-64 Linux (System V ABI)
 ; ============================================================================================ ;
 
@@ -13,22 +12,29 @@ SYS_READ	equ 0
 default	rel	; PIE-safe
 extern	__errno_location
 
-segment .text
+section .text
 	global	ft_read
 
 ft_read:
+	; rdi = fd
+	; rsi = buf
+	; rdx = count
 	mov rax, SYS_READ
 	syscall					; call system kernel
     cmp rax, 0				; if result < 0
     jl .error            	; set errno
 
 .return:
+	; rax = bytes read (success)
+	; rax = -1 (error)
 	ret
 
 .error:
-    neg rax                ; rax = -rax (= errno value)
-    mov edi, eax
+    neg rax				; rax = -rax (= errno value)
+	push rax			; save errno value to stack
+	; int * __errno_location(void)
     call __errno_location wrt ..plt
-    mov [rax], edi         ; *errno (4 bytes) = error number
-    mov rax, -1            ; return value = -1
+	pop rdx				; restore errno value from stack
+	mov [rax], rdx		; store errno value into *errno
+    mov rax, -1			; return value = -1
 	jmp .return
--- src/ft_write.s
+++ src/ft_write.s
@@ -4,7 +4,6 @@
 ;  Purpose   : Immitates (man 2 write)
 ;  Args      : rdi - output fd, rsi - ptr to str, rdx - number of chars to write
 ;  Returns   : rax = number of chars written
-;  Clobbers  : rax
 ;  Arch      : x86-64 Linux (System V ABI)
 ; ============================================================================================ ;
 
@@ -13,22 +12,29 @@ SYS_WRITE	equ 1
 default	rel	; PIE-safe
 extern	__errno_location
 
-segment .text
+section .text
 	global	ft_write
 
 ft_write:
+	; rdi = fd
+	; rsi = buf
+	; rdx = count
 	mov rax, SYS_WRITE
-	syscall					; call system kernel
-    cmp rax, 0				; if result < 0
-    jl .error            	; set errno
+	syscall				; call system kernel
+    cmp rax, 0			; if result < 0
+    jl .error          	; set errno
 
 .return:
+	; rax = bytes written (success)
+	; rax = -1 (error)
 	ret
 
 .error:
-    neg rax                ; rax = -rax (= errno value)
-    mov edi, eax
+    neg rax				; rax = -rax (= errno value)
+	push rax			; save errno value to stack
+	; int * __errno_location(void)
     call __errno_location wrt ..plt
-    mov [rax], edi         ; *errno (4 bytes) = error number
-    mov rax, -1            ; return value = -1
+	pop rdx				; restore errno value from stack
+	mov [rax], rdx		; store errno value into *errno
+    mov rax, -1			; return value = -1
 	jmp .return
